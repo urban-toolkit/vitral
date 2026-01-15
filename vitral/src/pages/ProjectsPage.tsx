818a77e9b029fbe295e25dc609d0ab526594158b
Gustavo Moreira <m.muller.gustavo@gmail.com>
2026-01-15 13:59:20 -0600

    Persistence + project management

--- vitral/src/pages/ProjectsPage.tsx
+++ vitral/src/pages/ProjectsPage.tsx
@@ -6,6 +6,8 @@ import { useNavigate } from "react-router-dom";
 
 import type { DocumentResponse } from "@/api/stateApi";
 
+import classes from './ProjectsPage.module.css'
+
 export function ProjectsPage() {
     const navigate = useNavigate();
 
@@ -22,15 +24,22 @@ export function ProjectsPage() {
     }, [])
 
     return (
-        <div>
-            {documents.map((document, _index) => {
-                return <div key={document.id}>
-                    <p>{document.title}</p>
-                    <p>{document.description}</p>
-                    <p>{document.id}</p>
-                    <button onClick={() => navigate("/project/"+document.id)}>Open</button>
+        <div className={classes.pageContainer}>
+            <div className={classes.innerContent}>
+                <p className={classes.title}>Projects</p>
+                <div className={classes.projectsGrid}>
+                    {documents.map((document, _index) => {
+                        return <div key={document.id} className={classes.projectCard}>
+                            <div className={classes.innerCard}>
+                                <p className={classes.documentTitle}>{document.title}</p>
+                                <p>{document.description}</p>
+                                <p>{document.id}</p>
+                                <button onClick={() => navigate("/project/"+document.id)}>Open</button>
+                            </div>
+                        </div>
+                    })}
                 </div>
-            })}
+            </div>
         </div>
     );
 }
